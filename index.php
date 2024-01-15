7dca890398218668b7f7a3aa32bfb02022be888d
miniece <mvelinda@icloud.com>
2024-01-14 19:49:58 -0500

    adding file

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "<h2>Name: Dakota Osborn and going for their Associates in Software Engineering. Their taking this course to increase their knowledge in web development. </h2>";
+        echo "<h2>They have taken courses and have beginner knowledge with C++, Java, HTML5, and SQL.</h2>";
+        echo "<h2>They have hobbies in artwork and design, as well as playing video games.</h2>";
+        echo "<h2>They have plans to partake in 3D development for artwork and pursuing an internship. </h2>";
+        echo "<h2>They're always open to learning new methods and procedures in both artwork development and web design, as well as software development.</h2>";
+        ?>
+    </body>
+</html>
